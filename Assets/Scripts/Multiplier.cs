48cedb078a470c2db569c34cb454b9f3ac977396
PavellMalakhov <127777612+PavellMalakhov@users.noreply.github.com>
2024-05-20 22:31:53 +0500

    Upload

--- Assets/Scripts/Multiplier.cs
+++ Assets/Scripts/Multiplier.cs
@@ -1,39 +1,54 @@
 using UnityEngine;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(MeshRenderer))]
 
 public class Multiplier : MonoBehaviour
 {
-    [SerializeField] private GameObject _cubePrefab;
     [SerializeField] private int _minValueClons;
     [SerializeField] private int _maxValueClons;
+    [SerializeField] private int _chanceSplit;
 
-    private float _explosionRadius = 1;
     private float _explosionForce = 300;
+    private float _explosionRadius = 1;
 
     private void OnMouseUpAsButton()
     {
         int multiplier = 2;
-        Vector3 size = gameObject.transform.localScale / multiplier;
+        int chanceSplitMaxValue = 101;
+        int chanceSplitMinValue = 0;
 
-        for (int i = 0; i < Random.Range(_minValueClons, _maxValueClons + 1); i++)
+        bool isSplit = Random.Range(chanceSplitMinValue, chanceSplitMaxValue) <= _chanceSplit;
+        int cubeValue = Random.Range(_minValueClons, _maxValueClons);
+
+        List<GameObject> cubes = new();
+
+        if (isSplit)
         {
-            _cubePrefab.transform.localScale = size;
-
-            Instantiate(_cubePrefab);
+            for (int i = 0; i < cubeValue; i++)
+            {
+                cubes.Add(Instantiate(gameObject));
+            }
         }
 
-        Collider[] hits = Physics.OverlapSphere(gameObject.transform.position, size.x / multiplier);
-
-        foreach (var item in hits)
+        foreach (var cube in cubes)
         {
-            item.GetComponent<Multiplier>()._maxValueClons /= multiplier;
-            item.GetComponent<Multiplier>()._minValueClons /= multiplier;
-
-            item.gameObject.GetComponent<MeshRenderer>().material.color = GetColor();
-
-            item.attachedRigidbody.AddExplosionForce(_explosionForce, transform.position, _explosionRadius);
+            Init(cube, multiplier);
         }
 
         Destroy(gameObject);
+    }
+
+    private void Init(GameObject cube, int multiplier)
+    {
+        cube.transform.localScale = transform.localScale / multiplier;
+
+        cube.GetComponent<Multiplier>()._chanceSplit /= multiplier;
+
+        cube.GetComponent<MeshRenderer>().material.color = GetColor();
+
+        cube.GetComponent<Rigidbody>().AddExplosionForce(_explosionForce, transform.position, _explosionRadius);
     }
 
     private Color GetColor()
